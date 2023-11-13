c86408935641d767b2bb2ca3a681570f5edb607e
mariusz.jachymczak <mariusz.jachymczak@gazeta.pl>
2023-11-13 23:05:38 +0100

    logic and styling added

---
+++ src/test.tsx
@@ -0,0 +1,64 @@
+import Typography from "@mui/material/Typography";
+import Markdown from "./components/markdown/Markdown";
+import blogPostContent from "../src/components/posts/blog-post1.md";
+import { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+
+function MarkdownListItem(props: any) {
+  return <Box component="li" sx={{ mt: 1, typography: "body1" }} {...props} />;
+}
+
+const options = {
+  overrides: {
+    h1: {
+      component: Typography,
+      props: {
+        gutterBottom: true,
+        variant: "h4",
+        component: "h1",
+      },
+    },
+    h2: {
+      component: Typography,
+      props: { gutterBottom: true, variant: "h6", component: "h2" },
+    },
+    h3: {
+      component: Typography,
+      props: { gutterBottom: true, variant: "subtitle1" },
+    },
+    h4: {
+      component: Typography,
+      props: {
+        gutterBottom: true,
+        variant: "caption",
+        paragraph: true,
+      },
+    },
+    p: {
+      component: Typography,
+      props: { paragraph: true },
+    },
+    a: { component: Link },
+    li: {
+      component: MarkdownListItem,
+    },
+  },
+};
+
+export function MyComponent(props: any) {
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    fetch(blogPostContent)
+      .then((response) => response.text())
+      .then((text) => setContent(text));
+  }, []);
+
+  return (
+    <Markdown options={options} {...props}>
+      {content}
+    </Markdown>
+  );
+}
+
+export default MyComponent;
